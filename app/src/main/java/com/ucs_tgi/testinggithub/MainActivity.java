e70a44628065aaf789ede073ce0d0077e09a20ff
Sai Zayar Htet <53620449+SaiZayarHtet1999@users.noreply.github.com>
2020-02-07 21:25:03 +0630

    GotoLoginPage

--- app/src/main/java/com/ucs_tgi/testinggithub/MainActivity.java
+++ app/src/main/java/com/ucs_tgi/testinggithub/MainActivity.java
@@ -2,7 +2,12 @@ package com.ucs_tgi.testinggithub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ucs_tgi.testinggithub.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //vjhgfgh
+        Button btn =findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         //I am sai zayar htet
 
