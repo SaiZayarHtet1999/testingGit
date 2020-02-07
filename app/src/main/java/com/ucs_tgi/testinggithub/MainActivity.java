341fe057f864b4718fd03fc2c8880b7bdc9b7356
Thiha-aung-eng <58001832+Thiha-aung-eng@users.noreply.github.com>
2020-02-07 21:11:05 +0630

    Hello version 1

--- app/src/main/java/com/ucs_tgi/testinggithub/MainActivity.java
+++ app/src/main/java/com/ucs_tgi/testinggithub/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //vjhgfgh
 
         //I am sai zayar htet
 
